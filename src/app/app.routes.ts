611f1acc4d7490d0cc6ea1f8f154fb2dc95ca854
Andrew De Lange <andrew2142@example.com>
2025-08-10 11:40:36 +0200

    --publish

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,32 @@
 import { Routes } from '@angular/router';
+import { Home } from './components/home/home';
+import { About } from './components/about/about';
+import { Pricing } from './components/pricing/pricing';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    component: Home,
+    title: 'Slim Reviews - Home'
+  },
+  {
+    path: 'about',
+    component: About,
+    title: 'Slim Reviews - About'
+  },
+ 
+  {
+    path: 'pricing',
+    component: Pricing,
+    title: 'Slim Reviews - Pricing'
+  },
+  {
+    path: '**',
+    redirectTo: '/home'
+  }
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,17 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Footer } from './components/footer/footer';
-import { Home } from './components/home/home';
 
 @Component({
   selector: 'app-root',
-  
   styleUrl: './app.scss',
-  imports: [Home, Footer],
-  template: `
-    <app-home />
-    <app-footer />
-  `,
+  imports: [RouterOutlet],
+  templateUrl: './app.html',
 })
 export class App {
   protected readonly title = signal('portfolio');
