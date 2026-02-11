42cf9088ce40cef999e7d9dbbc8ff622f56899f7
Andy <andrew.delange@sudosky.com>
2026-02-11 18:57:22 +0200

    --changes

--- src/app/components/gallery/gallery.ts
+++ src/app/components/gallery/gallery.ts
@@ -23,11 +23,16 @@ export class Gallery {
   selectedIndex: number | null = null;
   isMobile = false;
   rawYoutubeLinks = [
- 
+    'https://www.youtube.com/watch?v=87k56iOAfJk',
     'https://www.youtube.com/watch?v=QeSego_7Iek',
-    'https://www.youtube.com/watch?v=yM4IYDv6mtU',
-    
-    'https://www.youtube.com/watch?v=UGKAQjK8FGQ'
+    'https://www.youtube.com/shorts/vmSNrG5wUvU',
+    'https://www.youtube.com/watch?v=0K2B0WbiUxg',
+    'https://www.youtube.com/watch?v=oJf1RzGlLEs',
+    'https://www.instagram.com/reel/DMpNwj3suwt/',
+    'https://www.youtube.com/watch?v=wlMmS9RSCLg',
+    'https://www.youtube.com/watch?v=QUiyw9ZJTmQ',
+    'https://www.instagram.com/p/DT5gTRdDg-x/',
+    'https://youtube.com/shorts/GdzjxirIlOY?si=SQZ2M8Z8jst1IilR',
   ];
 
   videoEmbeds: SafeResourceUrl[] = [];
@@ -47,8 +52,22 @@ export class Gallery {
 
   constructor(private sanitizer: DomSanitizer){
      this.videoEmbeds = this.rawYoutubeLinks.map((url) => {
-      const videoId = new URL(url).searchParams.get('v');
-      const embedUrl = `https://www.youtube.com/embed/${videoId}`;
+      let embedUrl = '';
+      
+      if (url.includes('instagram.com')) {
+        // Instagram posts and reels
+        embedUrl = url.endsWith('/') ? `${url}embed` : `${url}/embed`;
+      } else if (url.includes('youtube.com/shorts/') || url.includes('youtu.be/shorts/')) {
+        // YouTube Shorts
+        const shortId = url.split('/shorts/')[1].split('?')[0];
+        embedUrl = `https://www.youtube.com/embed/${shortId}`;
+      } else if (url.includes('youtube.com/watch') || url.includes('youtu.be/')) {
+        // Standard YouTube videos
+        const urlObj = new URL(url);
+        const videoId = urlObj.searchParams.get('v') || urlObj.pathname.slice(1);
+        embedUrl = `https://www.youtube.com/embed/${videoId}`;
+      }
+      
       return this.sanitizer.bypassSecurityTrustResourceUrl(embedUrl);
     });
   }
